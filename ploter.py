248dfe7bee66e62d0bbeb532e0f87c4b9b215528
dottyjunkie <dottyjunkie870407@gmail.com>
2020-04-13 13:46:08 +0800

    hello

--- ploter.py
+++ ploter.py
@@ -3,23 +3,34 @@ import numpy as np
 import serial
 import time
 
-t = np.arange(0, 10.1, 0.1)
+
 x = []
 y = []
 z = []
 tilt = []
 
-serdev = '/dev/ttyACM2'
+serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev, 115200)
-for i in range(0, 101):
+for i in range(0, 100):
 	line = s.readline()
 	
 	sline = line.decode().split(",")
-	x.append(float(sline[0]))
-	y.append(float(sline[1]))
-	z.append(float(sline[2]))
-	tilt.append(int(sline[3][0]))
+	print(i)
+	print("is")
+	print(sline)
+	if(len(sline)==4):
+		try:
+			x.append(float(sline[0]))
+			y.append(float(sline[1]))
+			z.append(float(sline[2]))
+			tilt.append(int(sline[3][0]))
+		except:
+			print(i)
 
+t = np.arange(0,float(len(x))/10, 0.1)
+print("-----------------")
+print(len(x))
+print(len(t))
 fig, ax = plt.subplots(2, 1)
 ax[0].plot(t, x, 'r', label = "X")
 ax[0].plot(t, y, 'g', label = "Y")
